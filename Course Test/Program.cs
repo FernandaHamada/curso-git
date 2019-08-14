7633c88890a9ec1caf072ef7ab67bb54aecf9d47
Fernanda Hamada <fernandakubayashi@gmail.com>
2019-08-14 17:12:20 -0300

    Acrescentada mensagem de despedida

--- Course Test/Program.cs
+++ Course Test/Program.cs
@@ -7,9 +7,9 @@ namespace Course_Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-          
             Console.WriteLine("how are you today");
             Console.WriteLine("Tenha um bom dia");
+            Console.WriteLine("Tchau!");
 
             Console.ReadLine();
         }
