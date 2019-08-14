5c1a93976827df08895e7f77e318a6136c9488d0
Fernanda Hamada <fernandakubayashi@gmail.com>
2019-08-14 09:41:38 -0300

    Projeto iniciado, mostrando a mensagem Hello World

---
+++ Course Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Course_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
